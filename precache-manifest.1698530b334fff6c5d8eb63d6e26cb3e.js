3cf98c179721347fe1668dd39711960bff5d8c66
Nikolas Mach <nikolas.mach@ibm.com>
2019-11-21 16:32:41 +0100

    Updates

---
+++ precache-manifest.1698530b334fff6c5d8eb63d6e26cb3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f3def31944e6022f7cc23b258f8c32",
+    "url": "/ReactSample/index.html"
+  },
+  {
+    "revision": "ee633df9d1fda7d0ba54",
+    "url": "/ReactSample/static/css/main.0c528671.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/ReactSample/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "ee633df9d1fda7d0ba54",
+    "url": "/ReactSample/static/js/main.af4b0a3b.chunk.js"
+  },
+  {
+    "revision": "f0294b96bac96293a045",
+    "url": "/ReactSample/static/js/runtime-main.79e75e57.js"
+  }
+]);
